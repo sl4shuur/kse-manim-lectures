81017c9ac21e1ef34456dde308d5015dd7ac3bf7
Denys Mykhailov <dmykhailov@kse.org.ua>
2025-09-20 11:54:37 +0300

    feat: PoseSwitcher scene to toggle between different Sprite poses

--- render.py
+++ render.py
@@ -1,15 +1,19 @@
-from src.animations_sprites.manim_sprtites import Sprite
+from src.animations_sprites.manim_sprtites import PoseSwitcher
 
 if __name__ == "__main__":
     import os
     from pathlib import Path
 
-    scene_to_render = Sprite.__name__
-    file_name = Sprite.__module__.replace(".", "/") + ".py"
+    scene_to_render = PoseSwitcher.__name__
+    file_name = PoseSwitcher.__module__.replace(".", "/") + ".py"
     print(f"Rendering scene: {scene_to_render}")
 
     project_dir = Path(__file__).resolve().parent
-    command = f'docker run -it --rm -v "{project_dir}:/manim" -w /manim manimcommunity/manim manim {file_name} {scene_to_render} -ql'
+    command = (
+        f'docker run -it --rm -v "{project_dir}:/manim" -w /manim '
+        f"-e PYTHONPATH=/manim "
+        f"manimcommunity/manim manim {file_name} {scene_to_render} -ql"
+    )
     print(f"Running command: {command}")
 
     os.system(command)
--- src/animations_sprites/manim_sprtites.py
+++ src/animations_sprites/manim_sprtites.py
@@ -1,9 +1,29 @@
-from manim import * # type: ignore
+from manim import *  # type: ignore
+from src.animations_sprites.ManimSprite import ManimSprite
+from src.utils.config import POSES_NUM_LIST
+
+# ["04", "05", "06", "14", "15", "18", "19", "24"]
+# 04 - 18
+# 05 - 06 - 15 - 19
 
 
-class Sprite(Scene):
+class PoseSwitcher(Scene):
     def construct(self):
-        sprite = SVGMobject("assets/sprites_poses/adventurer_vector/pose_01.svg")
-        self.play(DrawBorderThenFill(sprite))
-        self.wait(1)
+        sprite = ManimSprite("adventurer")
 
+        # for loop to check all poses changing animations
+        for i in range(len(POSES_NUM_LIST)):
+            for j in range(len(POSES_NUM_LIST)):
+                if i < j:
+                    sprite.change_pose(POSES_NUM_LIST[i])
+                    sprite.change_pose(POSES_NUM_LIST[j])
+                    init_pose = sprite.old_manim_svgmobject
+                    cur_pose = sprite.cur_manim_svgmobject
+                    text = Text(
+                        f"Pose {POSES_NUM_LIST[i]} to {POSES_NUM_LIST[j]}"
+                    ).to_edge(UP)
+
+                    self.add(init_pose, text)
+                    self.play(Transform(init_pose, cur_pose), run_time=1)
+                    self.clear()
+                    self.wait(0.2)
